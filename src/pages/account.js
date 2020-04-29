928785f7bfaf5cdcae1111bbd4302be638cd607f
Ekwuno Obinna <obinnacodes@gmail.com>
2020-04-29 02:04:40 +0100

    I swear I don't know what is going on here

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,27 +1,36 @@
 import React from "react"
-import {Router} from "@reach/router"
+import { Router } from "@reach/router"
+import { login, isAuthenticated, getProfile } from "../utils/auth"
 import { Link } from "gatsby"
 
-const Home = ()=> <p> This is the Home page </p>
-const Settings = ()=> <p> This is the Settings page</p>
-const Billing = ()=> <p> This is the Billing page</p>
+const Home = ({ user }) => {
+  return <p>Hi, {user.name ? user.name : "friend"}!</p>
+}
+const Settings = () => <p>Settings</p>
+const Billing = () => <p>Billing</p>
 
-const Account = () => (
-  <>
-    <nav>
-        <Link to ="/account">Home </Link>
-        <Link to ="/account/settings">Settings </Link>
-        <Link to = "/account/billing"> Billing</Link>
-    </nav>
+const Account = () => {
+  if (!isAuthenticated()) {
+    login()
+    return <p>Redirecting to login...</p>
+  }
 
-    <Router>
+  const user = getProfile()
 
-      <Home path="/account" />
-      <Settings path="/account/settings" />
-      <Billing path="/account/billing" />
-    </Router>
-
-  </>
-)
+  return (
+    <>
+      <nav>
+        <Link to="/account/">Home</Link>{" "}
+        <Link to="/account/settings/">Settings</Link>{" "}
+        <Link to="/account/billing/">Billing</Link>{" "}
+      </nav>
+      <Router>
+        <Home path="/account/" user={user} />
+        <Settings path="/account/settings" />
+        <Billing path="/account/billing" />
+      </Router>
+    </>
+  )
+}
 
 export default Account
